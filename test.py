e56edd23482309799e9376745721d2ac26b568b1
park-ing <yh.park8742@gmail.com>
2022-04-25 21:35:40 +0900

    new

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-print("Computer Vision")
-print("test")
-print("hhyy")
+import collections
+
+
+def solution(participant, completion):
+    print(collections.Counter(participant))
+    print(collections.Counter(completion))
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    print(answer.keys())
+    return list(answer.keys())[0]
+
+x = ['a','b','c','d','e']
+t = ['b','e','a','c']
+print(solution(x,t))
+
+print()
